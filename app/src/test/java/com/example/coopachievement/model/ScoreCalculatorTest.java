ebb0bde57950ddba8959c74bafd04d35828f9c6b
frase <fgw@sfu.ca>
2022-11-21 13:15:44 -0800

    Testing functions initial set up

---
+++ app/src/test/java/com/example/coopachievement/model/ScoreCalculatorTest.java
@@ -0,0 +1,82 @@
+package com.example.coopachievement.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScoreCalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void editMatch() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDifficulty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setDifficulty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setPoorScore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPoorScore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setGreatScore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getGreatScore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setMatchName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMatchName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMatchesPlayed() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setDate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setNumPlayers() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNumPlayers() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setScore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getScore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void difficultyMultiplier() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setAchievementLevel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void fillLevelsList() {
+    }
+}
